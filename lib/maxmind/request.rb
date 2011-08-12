e463cc5478e78fcdf1ed7e48c3fb1cdfa7f2e2f8
MihailDV <mihaildv@gmail.com>
2011-08-12 16:20:14 +0300

    added ability to transfer settings for http connection

--- lib/maxmind/request.rb
+++ lib/maxmind/request.rb
@@ -1,25 +1,37 @@
 module Maxmind
   class Request
     # Required Fields
-    attr_accessor :client_ip, :city, :region, :postal, :country, :license_key
-    
+    attr_accessor :client_ip, :city, :region, :postal, :country, :license_key, :http
+
     # Optional Fields
     attr_accessor :domain, :bin, :bin_name, :bin_phone, :cust_phone, :requested_type,
       :forwarded_ip, :email, :username, :password, :transaction_id, :session_id,
       :shipping_address, :shipping_city, :shipping_region, :shipping_postal,
       :shipping_country, :user_agent, :accept_language
-    
-    def initialize(license_key, options = {}) 
+
+    def initialize(license_key, options = {}, http_settings = {})
       @license_key = license_key
-      
+
       options.each do |k, v|
         self.instance_variable_set("@#{k}", v)
       end
+
+      {:use_ssl => true}.merge(http_settings).each_pair do |k, v|
+        http.send("#{k.to_s}=", v)
+      end
     end
-    
+
+    def url
+      @url ||= URI.parse("https://minfraud1.maxmind.com/app/ccv2r")
+    end
+
+    def http
+      @http ||= Net::HTTP.new(url.host, url.port)
+    end
+
     def query(string = false)
       validate
-      
+
       required_fields = {
         :i                => @client_ip,
         :city             => @city,
@@ -28,7 +40,7 @@ module Maxmind
         :country          => @country,
         :license_key      => @license_key
       }
-      
+
       optional_fields = {
         :domain           => @domain,
         :bin              => @bin,
@@ -50,36 +62,33 @@ module Maxmind
         :user_agent       => @user_agent,
         :accept_language  => @accept_langage
       }
-      
+
       query = required_fields.merge(optional_fields)
       if string == false
-        return get(query.reject {|k, v| v.nil? }.to_query)    
+        return get(query.reject {|k, v| v.nil? }.to_query)
       else
         return query.reject {|k, v| v.nil? }.to_query
       end
     end
-    
+
     def get(query)
-      url = URI.parse("https://minfraud1.maxmind.com/app/ccv2r")
       req = Net::HTTP::Get.new("#{url.path}?#{query}")
-      h = Net::HTTP.new(url.host, url.port)
-      h.use_ssl = true
-      response = h.start { |http| http.request(req) }
+      response = http.start { |h| h.request(req) }
       return response.body
     end
-    
+
     def email=(email)
       @email = Digest::MD5.hexdigest(email.downcase)
     end
-    
+
     def username=(username)
       @username = Digest::MD5.hexdigest(username.downcase)
     end
-    
+
     def password=(password)
       @password = Digest::MD5.hexdigest(password.downcase)
     end
-    
+
     protected
     def validate
       raise ArgumentError, 'license key required' if @license_key.nil?
--- test/maxmind_test.rb
+++ test/maxmind_test.rb
@@ -1,36 +1,41 @@
 require 'test_helper'
 
 REQUIRED_FIELDS = {
-  :client_ip => '24.24.24.24', 
-  :city => 'New York', 
-  :region => 'NY', 
-  :postal => '11434', 
-  :country => 'US'
+  :client_ip => '24.24.24.24',
+  :city      => 'New York',
+  :region    => 'NY',
+  :postal    => '11434',
+  :country   => 'US'
 }
 
 RECOMMENDED_FIELDS = {
-  :domain => 'yahoo.com',
-  :bin => '549099',
+  :domain       => 'yahoo.com',
+  :bin          => '549099',
   :forwarded_ip => '24.24.24.25',
-  :email => 'test@test.com',
-  :username => 'test_carder_username',
-  :password => 'test_carder_password'
+  :email        => 'test@test.com',
+  :username     => 'test_carder_username',
+  :password     => 'test_carder_password'
 }
 
 OPTIONAL_FIELDS = {
-  :bin_name => 'MBNA America Bank',
-  :bin_phone => '800-421-2110',
-  :cust_phone => '212-242',
-  :requested_type => 'premium',
+  :bin_name         => 'MBNA America Bank',
+  :bin_phone        => '800-421-2110',
+  :cust_phone       => '212-242',
+  :requested_type   => 'premium',
   :shipping_address => '145-50 157th Street',
-  :shipping_city => 'Jamaica',
-  :shipping_region => 'NY',
-  :shipping_postal => '11434',
+  :shipping_city    => 'Jamaica',
+  :shipping_region  => 'NY',
+  :shipping_postal  => '11434',
   :shipping_country => 'US',
-  :transaction_id => '1234',
-  :session_id => 'abcd9876',
-  :user_agent => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1',
-  :accept_language => 'en-us'
+  :transaction_id   => '1234',
+  :session_id       => 'abcd9876',
+  :user_agent       => 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_5; en-us) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.2 Safari/525.20.1',
+  :accept_language  => 'en-us'
+}
+
+HTTP_SETTINGS = {
+  :open_timeout  => 5,
+  :read_timeout  => 10
 }
 
 class MaxmindTest < Test::Unit::TestCase
@@ -38,92 +43,112 @@ class MaxmindTest < Test::Unit::TestCase
     setup do
       @request = Maxmind::Request.new('key', REQUIRED_FIELDS)
     end
-    
+
     should "require a key" do
       lambda { Maxmind::Request.new }.should raise_error(ArgumentError)
-      
+
       Maxmind::Request.new('key').license_key.should == 'key'
     end
-    
+
     should "require client IP" do
       lambda { @request.client_ip = nil; @request.query }.should raise_error(ArgumentError)
     end
-    
+
     should "require city" do
       lambda { @request.city = nil; @request.query }.should raise_error(ArgumentError)
     end
-    
+
     should "require region" do
       lambda { @request.region = nil; @request.query }.should raise_error(ArgumentError)
     end
-    
+
     should "require postal" do
       lambda { @request.postal = nil; @request.query }.should raise_error(ArgumentError)
     end
-    
+
     should "require country" do
       lambda { @request.country = nil; @request.query }.should raise_error(ArgumentError)
     end
-    
+
     should "convert username to MD5" do
       @request.username = 'testuser'
       @request.username.should == '5d9c68c6c50ed3d02a2fcf54f63993b6'
     end
-    
+
     should "convert password to MD5" do
       @request.password = 'testpassword'
       @request.password.should == 'e16b2ab8d12314bf4efbd6203906ea6c'
     end
-    
+
     should "convert email to MD5" do
       @request.email = 'test@test.com'
       @request.email.should == 'b642b4217b34b1e8d3bd915fc65c4452'
     end
+
+    should "have http use ssl" do
+      @request.http.use_ssl = true
+    end
+
+    context "when HTTP settings are present" do
+      setup do
+        @request = Maxmind::Request.new('key', REQUIRED_FIELDS, HTTP_SETTINGS)
+      end
+
+      should "have http read timeout" do
+        @request.http.read_timeout = HTTP_SETTINGS[:read_timeout]
+      end
+
+      should "have http open timeout" do
+        @request.http.open_timeout = HTTP_SETTINGS[:open_timeout]
+      end
+    end
+
   end
-  
+
   #context "Requesting" do
   #  setup do
   #    request = Maxmind::Request.new('LICENSE_KEY', REQUIRED_FIELDS.merge(RECOMMENDED_FIELDS).merge(OPTIONAL_FIELDS))
   #    FakeWeb.register_uri(:get, "http://minfraud3.maxmind.com/app/ccv2r?" + request.query(true), :string => File.read(File.join(File.dirname(__FILE__), "fixtures/basic.txt")))
-  #    
-  #    @response = Maxmind::Response.new(request) 
+  #
+  #    @response = Maxmind::Response.new(request)
   #  end
   #end
-  
+
   context "Response" do
     setup do
-      request = Maxmind::Request.new('LICENSE_KEY', REQUIRED_FIELDS.merge(RECOMMENDED_FIELDS).merge(OPTIONAL_FIELDS))     
+      request = Maxmind::Request.new('LICENSE_KEY', REQUIRED_FIELDS.merge(RECOMMENDED_FIELDS).merge(OPTIONAL_FIELDS))
       FakeWeb.register_uri(:get, "https://minfraud1.maxmind.com/app/ccv2r?" + request.query(true), :body => File.read(File.join(File.dirname(__FILE__), "fixtures/response.txt")))
-      
-      @response = Maxmind::Response.new(request.query) 
+
+      @response = Maxmind::Response.new(request.query)
     end
-    
+
     should "require a response" do
       lambda { Maxmind::Response.new }.should raise_error(ArgumentError)
     end
-    
+
     should "have a distance" do
       @response.distance.should == 329
     end
-    
+
     should "have a maxmind ID" do
       @response.maxmind_id.should == '9VSOSDE2'
     end
-    
+
     should "have a risk score" do
       @response.risk_score.should == 2.0
     end
-    
+
     should "have a score" do
       @response.score.should == 7.66
     end
-    
+
     should "have queries remaining" do
       @response.queries_remaining.should == 955
     end
-    
+
     should "have an explanation" do
       @response.explanation.should_not == nil
     end
   end
+
 end
